6ceaa2e823273449ba625d88d5e3047ea1a8c8f0
finleyconway <finleyconway03@gmail.com>
2024-03-11 14:36:11 +0000

    Upscaled game

--- src/Sprite.py
+++ src/Sprite.py
@@ -3,23 +3,30 @@ import pygame
 class Sprite(pygame.sprite.Sprite):
     def __init__(self, spritePath, spriteType: str):
         pygame.sprite.Sprite.__init__(self)
-        
+
+        scalingFactor = 2
+
         self.originalImage = pygame.image.load(spritePath).convert_alpha()
-        self.image = self.originalImage
+        self.imageA = pygame.transform.scale(self.originalImage, (int(self.originalImage.get_width() * scalingFactor),
+                                                                int(self.originalImage.get_height() * scalingFactor)))
+
+        self.image = pygame.transform.scale(self.originalImage, (int(self.originalImage.get_width() * scalingFactor),
+                                                                int(self.originalImage.get_height() * scalingFactor)))
         self.rect = self.image.get_rect()
-        
+
         self.isFlipX = False
         self.isFlipY = False
-        
+
         self.spriteType = spriteType
-        
-    
+
+
+
     def update(self, deltaTime):
         pass
-    
+
     def flip(self, horizontal = False, vertical = False):
         if (horizontal != self.isFlipX or vertical != self.isFlipY):
             self.isFlipX = horizontal
             self.isFlipY = vertical
-            
-            self.image = pygame.transform.flip(self.originalImage, horizontal, vertical)
+
+            self.image = pygame.transform.flip(self.imageA, horizontal, vertical)
--- src/main.py
+++ src/main.py
@@ -6,92 +6,92 @@ import random
 def main():
     pygame.init()
     pygame.font.init()
-    
+
     # init window
     windowTitle: str = "Game"
-    windowSize: pygame.Coordinate = [500, 500]
-    
+    windowSize: pygame.Coordinate = [1000, 1000]
+
     pygame.display.set_caption(windowTitle)
     windowSurface = pygame.display.set_mode(windowSize)
-    
+
     # main loop
     game_loop(windowSurface)
-    
+
     pygame.quit()
 
 def game_loop(windowSurface: pygame.Surface):
     isRunning: bool = True
-    
+
     clock = pygame.time.Clock()
     targetFPS: int = 60
-    
+
     textFont = pygame.font.SysFont('Arial', 30)
     aliveCounter = 0
-    
+
     gameObjects = pygame.sprite.Group()
-    
+
     gameObjects.add(GameObjects.Sprite("Assets/Background.png", "Background"))
-    
-    player = GameObjects.NinjaCat("Assets/Cat.png", [250, 250], gameObjects)
-    
+
+    player = GameObjects.NinjaCat("Assets/Cat.png", [500, 500], gameObjects)
+
     gameObjects.add(player)
     for i in range(5):
-        randomAttack = random.randint(0, 1)
+        randomAttack = random.randint(0, 1) 
         gameObjects.add(GameObjects.Dog("Assets/Dog.png", player, randomAttack))
-    
+
     # left
-    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [0, 140]))
-    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [0, 260]))
-    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [0, 380]))
-    
+    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [0, 280]))
+    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [0, 520]))
+    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [0, 760]))
+
     # middle
-    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [125, 140]))
-    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [125, 260]))
-    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [125, 380]))
-    
-    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [250, 140]))
-    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [250, 260]))
-    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [250, 380]))
-    
-    
-    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [375, 140]))
-    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [375, 260]))
-    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [375, 380]))
-    
+    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [250, 280]))
+    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [250, 520]))
+    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [250, 760]))
+
+    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [500, 280]))
+    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [500, 520]))
+    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [500, 760]))
+
+
+    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [750, 280]))
+    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [750, 520]))
+    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [750, 760]))
+
     # right
-    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [468, 140]))
-    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [468, 260]))
-    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [468, 380]))
-    
+    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [935, 280]))
+    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [935, 520]))
+    gameObjects.add(GameObjects.Platform("Assets/Platform.png", player, [935, 760]))
+
     dogTimer = 10
     timer = 0
-    
+
     # game loop
     while (isRunning):
         deltaTime = clock.tick(targetFPS) / 1000
-        
+
         if (player.hasMoved):
             aliveCounter += deltaTime
-            
+
             timer += deltaTime
             if (timer >= dogTimer):
                 randomAttack = random.randint(0, 1)
                 gameObjects.add(GameObjects.Dog("Assets/Dog.png", player, randomAttack))
                 timer = 0
-        
-        # query poll events 
+
+        # query poll events
         for  event in pygame.event.get():
             if (event.type == pygame.QUIT):
                 isRunning = False
-        
+
         # update game objects
         update(deltaTime, gameObjects)
-        
+
         # render game objects
-        render(windowSurface, gameObjects, textFont, aliveCounter)     
-        
+        render(windowSurface, gameObjects, textFont, aliveCounter)
+
 def update(deltaTime: float, gameObjects):
-    gameObjects.update(deltaTime) 
+    gameObjects.update(deltaTime)
 
 def render(surface: pygame.Surface, gameObjects, textFont, aliveCounter):
     # clear screen
@@ -101,10 +101,10 @@ def render(surface: pygame.Surface, gameObjects, textFont, aliveCounter):
     gameObjects.draw(surface)
 
     textSurface = textFont.render(f"Score: {int(aliveCounter)}", False, (0, 0, 0))
-    surface.blit(textSurface, (10, 10))   
+    surface.blit(textSurface, (10, 10))
 
     # clear screen
-    pygame.display.flip()    
+    pygame.display.flip()
 
 if (__name__ == "__main__"):
     main()
